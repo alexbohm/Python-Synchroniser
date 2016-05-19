23e463b725a247d799aa3b86f02e9a6251874362
Alex Bohm <aabohm7@gmail.com>
2016-05-18 19:37:02 -0500

    Add files via upload

--- syncro.py
+++ syncro.py
@@ -20,21 +20,13 @@ class single(object):
 			self.state = False
 			self.img = PhotoImage(file="%s/icons/button_off.gif" % (cwd))
 			self.but.configure(image=self.img)
-	def active(self):
-		if self.state==True:
-			self.img = PhotoImage(file="%s/icons/button_on_highlight.gif" % (cwd))
-			self.but.configure(image=self.img)
-		else:
-			self.img = PhotoImage(file="%s/icons/button_off_highlight.gif" % (cwd))
-			self.but.configure(image=self.img)
-	def deactive(self):
-		if self.state==True:
-			self.img = PhotoImage(file="%s/icons/button_on.gif" % (cwd))
-			self.but.configure(image=self.img)
-		else:
-			self.img = PhotoImage(file="%s/icons/button_off.gif" % (cwd))
-			self.but.configure(image=self.img)
-
+	def active(self, st=False):
+		high = ""
+		state = "off"
+		if self.state==True: state = "on"
+		if st==True: high = "_highlight"
+		self.img = PhotoImage(file="%s/icons/button_%s%s.gif" % (cwd,state,high))
+		self.but.configure(image=self.img)
 class row(object):
 	def __init__(self, row=0, length=5):
 		self.snd = Sound()
@@ -58,18 +50,13 @@ class row(object):
 		for num in range(5,length+5):
 			self.buts[num] = single([row,num])
 	def l_file(self):
-		self.rem()
 		fname = fd.askopenfilename(filetypes=[("Audio Files",".wav")])
-		if fname != "":
-			self.snd.read(fname)
-			self.open.configure(state=DISABLED)
+		if fname != "": self.snd.read(fname); self.open.configure(state=DISABLED)
 	def add(self):
 		if self.length+1 > len(self.buts): self.buts[self.length+5] = single([self.row, self.length+5])
 		else: self.buts[self.length+5].but.grid(row=self.row, column=self.length+5)
 		self.length+=1
-	def rem(self):
-		self.buts[self.length+4].but.grid_remove()
-		self.length -= 1
+	def rem(self): self.buts[self.length+4].but.grid_remove(); self.length -= 1
 	def mute(self):
 		if self.act:
 			self.act = False
@@ -81,13 +68,11 @@ class row(object):
 			self.m.configure(image=self.m_i)
 	def next(self):
 		if self.act:
-			self.buts[self.pos].deactive()
+			self.buts[self.pos].active(False)
 			self.pos += 1
-			if self.pos > self.length+4:
-				self.pos=5
-			if self.buts[self.pos].state==True:
-				self.snd.play()
-			self.buts[self.pos].active()
+			if self.pos > self.length+4: self.pos=5
+			if self.buts[self.pos].state==True: self.snd.play()
+			self.buts[self.pos].active(True)
 	def destroy(self):
 		self.act = False
 		self.des = True
@@ -115,9 +100,7 @@ class master(object):
 		self.sto = Button(win, text="Stop", command=lambda: self.stop())
 		self.sto.grid(row=len(self.rows), column=2)
 		self.add()
-	def stop(self):
-		self.st = True
-		self.s.configure(state=NORMAL)
+	def stop(self): self.st = True; self.s.configure(state=NORMAL)
 	def add(self):
 		ind = len(self.rows)+1
 		self.rows[ind] = row(ind)
@@ -127,15 +110,13 @@ class master(object):
 	def start(self):
 		self.s.configure(state=DISABLED)
 		for row in self.rows:
-			self.rows[row].buts[self.rows[row].pos].deactive()
+			self.rows[row].buts[self.rows[row].pos].active(False)
 			self.rows[row].pos = 5
 		self.st = False
 		self.loop()
 	def loop(self):
-		for row in self.rows:
-			self.rows[row].next()
-		if not self.st:
-			win.after(self.delay, lambda: self.loop())
+		for row in self.rows: self.rows[row].next()
+		if not self.st: win.after(self.delay, lambda: self.loop())
 
 master()
 win.mainloop()
